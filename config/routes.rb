61013d3f7cebcb7fcb564e978ea62bc3fd7886e5
2015E8007361074 <ieeflsyu@outlook.com>
2016-01-03 21:12:18 +0800

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :courses
+  resources :comments
+  resources :users
+  resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
